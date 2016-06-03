74023a49995762b363560f0754407ab2b1baeed9
borjengchensc <bor-jeng.chen@snapchat.com>
2016-06-03 16:24:53 -0700

    borjeng test commit

---
+++ palm_reading_borjeng/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  palm_reading_borjeng
+//
+//  Created by Bor-Jeng Chen on 6/3/16.
+//  Copyright © 2016 Bor-Jeng Chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
